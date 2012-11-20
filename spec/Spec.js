a38b4189313a34cb63f4288f87e96c96d9c7e1dd
Arne Fostvedt <arne@Arnes-MacBook-Air.local>
2012-11-19 16:18:14 -0800

    added factorial test

--- spec/Spec.js
+++ spec/Spec.js
@@ -1,1 +1,15 @@
 // Your Jasmine specs here
+describe("factorial", function() {
+  it("evaluates correctly for a single-digit number", function() {
+    expect(factorial(5)).toEqual(120);
+  });
+
+  it('evaluates correctly for a double-digit number', function() {
+    expect(factorial(11)).toEqual(39916800);
+  });
+
+  it('evaluates correctly for 0', function() {
+    expect(factorial(0)).toEqual(0);
+  });
+
+});
--- src/Script.js
+++ src/Script.js
@@ -1,1 +1,10 @@
 // Your JavaScript code here
+function factorial ( n ){
+  if (n === 1) { return n }
+  if (n === 0) { return n }
+  return n * factorial( n - 1);
+}
+
+console.log( factorial ( 5 ) );
+console.log( factorial ( 11 ) );
+console.log( factorial ( 0 ) );
